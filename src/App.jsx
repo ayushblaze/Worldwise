86e3fa08e39a72f2cf559c9da79b2a6f771a4836
ayushblaze <ayushmoca@gmail.com>
2025-04-11 22:57:16 +0530

    feat(countries): add component to display countries

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import { useEffect, useState } from "react";
+import CountryList from "./components/CountryList";
 
 const BASE_URL = 'http://localhost:8000';
 
@@ -45,8 +46,18 @@ function App() {
           <Route path="pricing" element={<Pricing />} />
           <Route path="login" element={<Login />} />
           <Route path="app" element={<AppLayout />}>
-            <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
-            <Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
+            <Route
+              index
+              element={<CityList cities={cities} isLoading={isLoading} />}
+            />
+            <Route
+              path="cities"
+              element={<CityList cities={cities} isLoading={isLoading} />}
+            />
+            <Route
+              path="countries"
+              element={<CountryList cities={cities} isLoading={isLoading} />}
+            />
             <Route path="countries" element={<p>List of countries</p>} />
             <Route path="form" element={<p>Form</p>} />
           </Route>
