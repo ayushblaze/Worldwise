a925240a87e324e483febdd37319e0d115d0aef8
ayushblaze <ayushmoca@gmail.com>
2025-05-01 22:59:27 +0530

    feat(map): create custom hook to extract position on click

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable */
 // "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=0&longitude=0"
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import styles from "./Form.module.css";
 import Button from "./Button";
 import BackButton from "./BackButton";
+import { useUrlPosition } from "../hooks/useUrlPosition";
 
 export function convertToEmoji(countryCode) {
   const codePoints = countryCode
@@ -15,11 +16,35 @@ export function convertToEmoji(countryCode) {
   return String.fromCodePoint(...codePoints);
 }
 
+const BASE_URL = 'https://api.bigdatacloud.net/data/reverse-geocode-client ';
+
 function Form() {
+  const [lat, lng] = useUrlPosition();
+  console.log("New Coordinates:", lat, lng);
+
+  const [isLoadingGeocoding, setIsLoadingGeocoding] = useState(false);
   const [cityName, setCityName] = useState("");
   const [country, setCountry] = useState("");
   const [date, setDate] = useState(new Date());
   const [notes, setNotes] = useState("");
+
+  useEffect(() => {
+    async function fetchCityData() {
+      try {
+        setIsLoadingGeocoding(true);
+        const res = await fetch(`${BASE_URL}?latitude=${lat}&longitude=${lng}`);
+        console.log(`${BASE_URL}?latitude=${lat}&longitude=${lng}`);
+        const data = await res.json();
+        console.log(data);
+      } catch (e) {
+        
+      } finally {
+        setIsLoadingGeocoding(false);
+      }
+    }
+    
+    fetchCityData();
+  }, [lat, lng]);
 
   return (
     <form className={styles.form}>
