804151447354ef6f48fff2b5c34818985f268b66
ayushblaze <ayushmoca@gmail.com>
2025-05-06 12:42:34 +0530

    feat(auth-context): change login creds

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -28,10 +28,10 @@ function reducer(state, action) {
 }
 
 const FAKE_USER = {
-  name: "Jack",
-  email: "jack@example.com",
+  name: "Ayush",
+  email: "ayush@worldwise.com",
   password: "qwerty",
-  avatar: "https://i.pravatar.cc/100?u=zz",
+  avatar: "https://i.ibb.co/0js20QcW/channelicon.jpg",
 };
 
 function AuthProvider({ children }) {
